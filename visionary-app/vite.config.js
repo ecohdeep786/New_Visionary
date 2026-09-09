5b8304b0052783853031009d634b3f616aca4550
Visionary <verification@engst.co.in>
2026-09-09 07:28:18 +0000

    version 1.0.21

--- visionary-app/vite.config.js
+++ visionary-app/vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    headers: securityHeaders,
+  headers: securityHeaders,
   },
   preview: {
     headers: productionSecurityHeaders,
